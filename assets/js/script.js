92559a21c74691867273d7ca1463181420e2e295
MalikTalha5524 <120513291+MalikTalha5524@users.noreply.github.com>
2023-05-06 23:44:14 +0500

    Cara

---
+++ assets/js/script.js
@@ -0,0 +1,65 @@
+// let a = 656;
+// function log_input( a)
+// {
+//     console.log(a);
+// }
+// console.log(a);
+// alert('Hello Person');
+// console.log('hello'); 
+// console.log(3434+2432);
+// log_input(a);
+// let Masn = new Date();
+// console.log(Masn);
+
+
+
+// console.log('This is a my pratices pro');
+// let talha = window.document;
+// window.alert(hello herry);
+// talha = prompt('type your name');
+// talha = confirm('Are you sure you want to delete this page');
+//  talha = location.toString;
+// talha = history;
+// console.log(talha); 
+
+
+
+
+// console.log('This is a my pratices on dom');
+// let a = document;
+// a =document.all;
+// a = document.body;
+// a = document.forms;
+
+// array.from(a).forEach(function(element){
+//     console.log(element);
+// });
+// a = document.links;
+// a = document.images;
+// a = document.scripts;
+
+
+// console.log(a);
+
+
+// console.log('This is a my pratices on exc');
+
+// // let a = document.all;
+//   let a = document.links;
+
+
+// console.log(a[0]);
+
+
+
+// let element = document.createElement('ul');
+// element.className = 'la';
+// console.log(element);
+
+
+let today = new Date();
+console.log(today);
+
+
+let otherDate = new Date("12-2-2015 4:50:30");
+console.log(otherDate);
